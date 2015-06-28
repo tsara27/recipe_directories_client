6e84d2b44b86b1e3f9cbc6e0c36471095d55b929
tsara27 <tsara.tf@gmail.com>
2015-06-28 14:15:14 +0700

    add routes for recipes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('recipes', { path: '/'});
+  this.route('recipe', { path: '/recipes/:recipe_id' });
 });
 
 export default Router;
