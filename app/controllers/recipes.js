1bef45abcb86c3db5a0b2269d12954b01cb9f14a
tsara27 <tsara.tf@gmail.com>
2015-07-24 20:36:39 +0700

    Add open modal and add book

--- app/controllers/recipes.js
+++ app/controllers/recipes.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  modalIsOpen: false,
+  actions: {
+    openModal: function() {
+      this.set('modalIsOpen', true);
+    },
+    closeModal: function() {
+      this.set('modalIsOpen', false);
+    },
+    addBook: function(){
+      var book = this.store.createRecord('recipe', {
+        title: this.get('bookTitle'),
+        description: this.get('bookDescription')
+      });
+      book.save();
+    }
+  }
 });
